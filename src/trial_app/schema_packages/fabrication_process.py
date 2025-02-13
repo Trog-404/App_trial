4f8c122cace763e785c0aea4e269878dd9299537
Trog-404 <bontornomatteo@gmail.com>
2025-02-13 09:13:00 +0000

    Ridotto all'osso il plugin

---
+++ src/trial_app/schema_packages/fabrication_process.py
@@ -0,0 +1,61 @@
+import datetime
+import os
+import random
+import re
+import time
+from typing import TYPE_CHECKING, Dict, Iterable, List
+
+import h5py
+import numpy as np
+
+import requests
+from ase.data import atomic_masses, atomic_numbers, chemical_symbols
+from unidecode import unidecode
+
+from nomad.datamodel.metainfo.workflow import Link, Task, TaskReference, Workflow
+from nomad.metainfo.data_type import m_str
+
+if TYPE_CHECKING:
+    from structlog.stdlib import (
+        BoundLogger,
+    )
+
+from nomad.datamodel.metainfo.annotations import ELNAnnotation
+from nomad.metainfo import Quantity, Section, SubSection
+from nomad.metainfo.basesections import ProcessStep, Process
+
+
+class FabricationStep(ProcessStep):
+    """
+    Any dependant step of an `Activity`.
+    """
+
+    m_def = Section()
+    fabricationEquipmentRecipeName = Quantity(
+        type=str,
+        description="""
+        A short and descriptive name for the recipe used.
+        """,
+        a_eln=ELNAnnotation(
+            component='StringEditQuantity',
+            label='recipe name',
+        ),
+    )
+
+class FabricationProcess(Process):
+    m_def = Section()
+    fabricationProductType = Quantity(
+        type=str,
+        description="""
+        A short and descriptive name for the product.
+        """,
+        a_eln=ELNAnnotation(
+            component='StringEditQuantity',
+            label='product name',
+        ),
+    )
+    steps = SubSection(
+        section_def = FabricationProcess,
+        repeats = True
+
+    )
